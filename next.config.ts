431176f1f82102f4fdaf78e0413ee0f46a41e9e0
Alizaib <alizaib43@gmail.com>
2026-05-05 16:32:21 +0500

    Update build config for static export

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.1.7", "192.168.*.*", "10.*.*.*", "172.16.*.*"],
+  output: "export",
+  basePath: "/Hospital-Device-Ecom-Web-3D",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
