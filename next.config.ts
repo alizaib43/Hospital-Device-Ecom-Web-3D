eacbdbde4636482d606b38022997f15734e54ddd
Alizaib <alizaib43@gmail.com>
2026-05-05 17:03:33 +0500

    Only use basePath in production for local dev convenience

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.1.7", "192.168.*.*", "10.*.*.*", "172.16.*.*"],
   output: "export",
-  basePath: "/Hospital-Device-Ecom-Web-3D",
+  basePath: isProd ? "/Hospital-Device-Ecom-Web-3D" : "",
   images: {
     unoptimized: true,
   },
